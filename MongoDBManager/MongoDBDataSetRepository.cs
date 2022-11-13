3b7a7df35ed783e1427956d07d7ab2eccf5ba3aa
Tom Vande Wiele <tvdewiel@hotmail.com>
2022-11-13 11:07:28 +0100

    find

--- MongoDBManager/MongoDBDataSetRepository.cs
+++ MongoDBManager/MongoDBDataSetRepository.cs
@@ -24,5 +24,33 @@ namespace MongoDBManager
             var collection = database.GetCollection<MongoDBDataSet>("datasets");
             collection.InsertMany(datasets);
         }
+        public List<MongoDBDataSet> FindDataSets(string campaignCode,DataSetType dataSetType,string dataSeries)
+        {
+            var collection = database.GetCollection<MongoDBDataSet>("datasets");
+            return collection.Find(x => x.metaInfo.CampaignCode == campaignCode 
+            && x.metaInfo.DataSetType==dataSetType 
+            && x.metaInfo.DataSeries== dataSeries).ToList();          
+        }
+        public List<MongoDBDataSet> FilterDataSets(string campaignCode, DataSetType dataSetType, string dataSeries)
+        {
+            var collection = database.GetCollection<MongoDBDataSet>("datasets");
+            var filter1 = Builders<MongoDBDataSet>.Filter.Eq(x => x.metaInfo.DataSetType, dataSetType);
+            var filter2 = Builders<MongoDBDataSet>.Filter.Eq(x => x.metaInfo.CampaignCode,campaignCode);
+            var filter3 = Builders<MongoDBDataSet>.Filter.Eq(x => x.metaInfo.DataSeries, dataSeries);
+            return collection.Find(filter1&filter2&filter3).ToList();
+        }
+        //public List<MongoDBDataSet> crudRead()
+        //{
+        //    var collection = database.GetCollection<MongoDBDataSet>("datasets");
+        //    var filter1 = Builders<MongoDBDataSet>.Filter.Eq(x => x.metaInfo.DataSetType, DataSetType.DataSet);
+        //    var filter2 = Builders<MongoDBDataSet>.Filter.Eq(x => x.metaInfo.CampaignCode, "Juli 2022");
+        //    var filter3 = Builders<MongoDBDataSet>.Filter.Eq(x => x.metaInfo.DataSeries, "ModelTest A");
+        //    var res = collection.Find(filter1 & filter2 & filter3).ToList();
+        //    //Console.WriteLine(res2);
+        //    ////methode 3
+        //    //var res3 = collection.AsQueryable<MongoDBDataSet>().Where(x => x.metaInfo.NrOfDataPoints > 2000).ToList();
+        //    //Console.WriteLine(res3);
+        //    return res;
+        //}
     }
 }
--- MongoDBManagerTest/Program.cs
+++ MongoDBManagerTest/Program.cs
@@ -3,23 +3,25 @@ using DataSetManager;
 using MongoDBManager;
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
 string source = @"C:\data\belgica\belgica.txt";
 string campaign = "Juli 2022";
 string dataSeries = "ModelTest A1";
-var data = FileDataSetManager.ReadData(source);
-var sets = FileDataSetManager.MakeDataSetsWithTestSet(data.data, new List<int> { 500, 1000, 2500, 50000, 100000 });
+//var data = FileDataSetManager.ReadData(source);
+//var sets = FileDataSetManager.MakeDataSetsWithTestSet(data.data, new List<int> { 500, 1000, 2500, 50000, 100000 });
 string connString = "mongodb://localhost:27017";
 MongoDBDataSetRepository repository = new MongoDBDataSetRepository(connString);
-List<MongoDBDataSet> mds = new List<MongoDBDataSet>();
-DataSetMetaInfo meta = new DataSetMetaInfo(sets[0].data.Count, source, campaign, dataSeries,DataSetType.TestSet);
-MongoDBDataSet mdsSet = new MongoDBDataSet(sets[0], meta);
-mds.Add(mdsSet);
-for (int i = 1; i < sets.Count; i++)
-{
-    meta = new DataSetMetaInfo(sets[i].data.Count, source, campaign,dataSeries, DataSetType.DataSet);
-    mdsSet = new MongoDBDataSet(sets[i], meta);
-    mds.Add(mdsSet);
-}
-repository.WriteDataSets(mds);
+//List<MongoDBDataSet> mds = new List<MongoDBDataSet>();
+//DataSetMetaInfo meta = new DataSetMetaInfo(sets[0].data.Count, source, campaign, dataSeries,DataSetType.TestSet);
+//MongoDBDataSet mdsSet = new MongoDBDataSet(sets[0], meta);
+//mds.Add(mdsSet);
+//for (int i = 1; i < sets.Count; i++)
+//{
+//    meta = new DataSetMetaInfo(sets[i].data.Count, source, campaign,dataSeries, DataSetType.DataSet);
+//    mdsSet = new MongoDBDataSet(sets[i], meta);
+//    mds.Add(mdsSet);
+//}
+//repository.WriteDataSets(mds);
+//var r=repository.crudRead();
+//var r=repository.FindDataSets(campaign,DataSetType.DataSet,dataSeries);
+var r = repository.FilterDataSets(campaign, DataSetType.DataSet, dataSeries);
 Console.WriteLine("end");
